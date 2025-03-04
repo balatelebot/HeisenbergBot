4787805664b04d04b1c9b1c67659bf6814803fa0
balatelebot <cutsandclips18@gmail.com>
2025-03-04 12:43:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,9 +28,9 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=50,
+            workers=500,
             plugins={"root": "plugins"},
-            sleep_threshold=5,
+            sleep_threshold=1,
         )
 
     async def start(self):
